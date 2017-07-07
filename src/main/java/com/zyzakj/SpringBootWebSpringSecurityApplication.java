34062a3b93e5274ae40cd71116a4461c96b1af67
jarekpc@o2.pl <ponar982fw>
2017-07-07 16:34:24 +0200

    added internationalization in project

--- src/main/java/com/zyzakj/SpringBootWebSpringSecurityApplication.java
+++ src/main/java/com/zyzakj/SpringBootWebSpringSecurityApplication.java
@@ -2,11 +2,34 @@ package com.zyzakj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
-public class SpringBootWebSpringSecurityApplication {
+public class SpringBootWebSpringSecurityApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebSpringSecurityApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.US); // Set default Locale as US
+		return slr;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("i18n/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
 	}
 }
